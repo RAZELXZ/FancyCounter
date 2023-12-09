5333b77974135acf8a6f7772c7f584d79fc5ef93
Qifeng Li <a879265509@hotmail.com>
2023-12-09 17:52:56 -0600

    Componenized

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Count from "./Components/Count";
+import CountButtons from "./Components/CountButtons";
+import ResetButton from "./Components/ResetButton";
+import Title from "./Components/Title";
+
 function App() {
-  return <p>Hello World! </p>;
+  return (
+    <main>
+      <div>
+        <Title />
+        <Count number={5} />
+        <ResetButton />
+        <CountButtons />
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Components/Title.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Title = () => {
+  return <h1>Fancy Counter</h1>;
+};
+
+export default Title;
